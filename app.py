9511dacb9871356de368bb9233b47ae928b9d5a6
Adityatomar28 <adityasinghtomar52gmail.com>
2026-03-26 04:03:55 +0530

    Update requirements and deployment fixes

--- app.py
+++ app.py
@@ -243,5 +243,7 @@ def predict():
 
     return render_template("predict.html")
 
+# if __name__ == "__main__":
+#     app.run(debug=True, port=5001)
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(host="0.0.0.0", port=5000)
